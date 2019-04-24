b1d89c308e5f82a6ff106b7074d23017611f69ed
Psalmfill <samfield4sure@gmail.com>
2019-04-24 19:38:10 +0100

    add menu and firebaseui dependency

--- app/src/main/java/com/samfieldhawb/instaapp/MainActivity.java
+++ app/src/main/java/com/samfieldhawb/instaapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.samfieldhawb.instaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 }
