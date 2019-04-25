e9a95100ce24b8d4b42deeae91916d897f7f4d44
Psalmfill <samfield4sure@gmail.com>
2019-04-25 15:57:52 +0100

    setup Post layout and StoragePreference

--- app/src/main/java/com/samfieldhawb/instaapp/PostActivity.java
+++ app/src/main/java/com/samfieldhawb/instaapp/PostActivity.java
@@ -6,17 +6,30 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
     public static final int IMAGE_REQUEST_CODE = 1;
+    private StorageReference mStorageReference;
     Uri mUri = null;
     ImageButton mImageButton;
+    private EditText mNameField,mDescriptionField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         mImageButton = findViewById(R.id.image_button);
+
+        mNameField = findViewById(R.id.edit_name);
+        mDescriptionField = findViewById(R.id.edit_description);
+        mStorageReference = FirebaseStorage.getInstance().getReference();
     }
 
     public void imageButtonClicked(View view){
@@ -33,5 +46,27 @@ public class PostActivity extends AppCompatActivity {
             mImageButton.setImageURI(mUri);
 
         }
+    }
+
+    public void submitButtonClicked(View view){
+        String name = mNameField.getText().toString().trim();
+        String desc = mDescriptionField.getText().toString().trim();
+
+        if(name.isEmpty()){
+            mNameField.setError("Name cannot e empty");
+            return;
+        }
+        if (desc.isEmpty()){
+            mDescriptionField.setError("Description cannot be empty");
+            return;
+        }
+        StorageReference filePath = mStorageReference.child("PostImages").child(mUri.getLastPathSegment());
+        filePath.putFile(mUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+              Uri downloadUrl = taskSnapshot.getUploadSessionUri();
+                Toast.makeText(PostActivity.this,"Upload Completed",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
